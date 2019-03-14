54e7233c97f4b2cc693aa8cfca459eb8f0e48aa5
Bryan <Bryan@BryanLaptop>
2019-03-13 23:47:33 -0400

    Created user class
    Only added the getters and setters for now

---
+++ Top Gamer/src/TopGamer/Business/User.java
@@ -0,0 +1,84 @@
+/**
+ * User class definition
+ * 
+ * 
+ * @author Bryan 
+ */
+
+package TopGamer.Business;
+
+public class User 
+{
+	public String m_firstName;
+	public String m_lastName;
+	public String m_email;
+	public String m_username;
+	public String m_password;
+	
+	/**
+	 * User constructor
+	 * 
+	 * Sets all members to default value: N/A
+	 */
+	public User() {
+		
+		m_firstName = "N/A";
+		m_lastName = "N/A";
+		m_email = "N/A";
+		m_username = "N/A";
+		m_password = "N/A";
+	}
+	
+	public void SetFirstName(String firstName)
+	{
+		m_firstName = firstName;
+	}
+	
+	public String GetFirstName()
+	{
+		return m_firstName;
+	}
+	
+	public void SetLastName(String lastName)
+	{
+		m_lastName = lastName;
+	}
+	
+	public String GetLastName()
+	{
+		return m_lastName;
+	}
+	
+	public void SetEmail(String email)
+	{
+		m_firstName = email;
+	}
+	
+	public String SetEmail()
+	{
+		return m_email;
+	}
+	
+	public void SetUsername(String username)
+	{
+		m_username = username;
+	}
+	
+	public String GetUsername()
+	{
+		return m_username;
+	}
+	
+	public void SetPassword(String pass)
+	{
+		m_password = pass;
+	}
+	
+	public String GetPassword()
+	{
+		return m_password;
+	}
+	
+	
+	
+}
--- Top Gamer/src/TopGamer/Business/package-info.java
+++ Top Gamer/src/TopGamer/Business/package-info.java
@@ -1,8 +1,9 @@
+
 /**
+ * Classes to store user and tournament data
  * 
- */
-/**
+ * @since 1.0
  * @author Bryan
- *
+ * @version 1.0
  */
 package TopGamer.Business;
