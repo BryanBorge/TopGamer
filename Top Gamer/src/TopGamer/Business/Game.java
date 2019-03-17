c1397c4751ab4d20f146b3f693031d934aec56b0
Bryan Borgesano <BryanBorgesano97@gmail.com>
2019-03-17 12:19:20 -0400

    Updated Game and Platform classes
    Added getters and setters to Game and Platform classes

--- Top Gamer/src/TopGamer/Business/Game.java
+++ Top Gamer/src/TopGamer/Business/Game.java
@@ -1,6 +1,64 @@
 package TopGamer.Business;
 
+import java.util.ArrayList;
+
 public class Game 
 {
-
+	private String m_gameName;
+	private Platform m_platform;
+	
+	/**
+	 * Game default constructor 
+	 * 
+	 * Set name and platform to default values
+	 */
+	public Game()
+	{
+		m_gameName = "N/A";
+		m_platform = new Platform();
+	}
+	
+	/**
+	 * Game constructor with parameters
+	 * @param name - Name of the game
+	 * @param platform - Name of the platform
+	 */
+	public Game(String name, String platform)
+	{
+		m_gameName = name;
+		m_platform.SetPlatform(platform);
+	}
+	
+	/**
+	 * SetGameName
+	 * Sets gameName member variable
+	 * @param name - Name of the game
+	 */
+	public void SetGameName(String name)
+	{
+		m_gameName = name;
+	}
+	
+	/**
+	 * GetGameName
+	 * Returns value of gameName member variable
+	 * @return gameName
+	 */
+	public String GetGameName()
+	{
+		return m_gameName;
+	}
+	
+	/**
+	 * SetGamePlatform
+	 * Sets platform member variable
+	 * @param platform - Platform of the game
+	 */
+	public void SetGamePlatform(String platform)
+	{
+		m_platform.SetPlatform(platform);
+	}
+	
+	
+	
 }
--- Top Gamer/src/TopGamer/Business/Platform.java
+++ Top Gamer/src/TopGamer/Business/Platform.java
@@ -1,5 +1,28 @@
 package TopGamer.Business;
 
-public class Platform {
 
+
+public class Platform 
+{
+	enum PlatformType
+	{
+		NOTAVAILABLE,PC,XBOX,PS4
+	}
+	
+	private PlatformType m_platform;
+	
+	public Platform()
+	{
+		m_platform = PlatformType.valueOf("NOTAVAILABLE");
+	}
+	
+	public void SetPlatform(String platform)
+	{
+		m_platform = PlatformType.valueOf(platform);
+	}
+	
+	public String GetPlatform()
+	{
+		return m_platform.toString();
+	}
 }
