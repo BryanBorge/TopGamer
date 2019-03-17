4134dd96f719529679511654d13dc91348a136f4
Bryan Borgesano <BryanBorgesano97@gmail.com>
2019-03-17 18:38:14 -0400

    Added to Gamer and Platform

--- Top Gamer/src/TopGamer/Business/Gamer.java
+++ Top Gamer/src/TopGamer/Business/Gamer.java
@@ -1,5 +1,9 @@
 package TopGamer.Business;
 
 public class Gamer {
+	
+	private String m_gamerTag;
+	private int m_wins;
+	private int m_losses;
 
 }
--- Top Gamer/src/TopGamer/Business/Platform.java
+++ Top Gamer/src/TopGamer/Business/Platform.java
@@ -4,23 +4,44 @@ package TopGamer.Business;
 
 public class Platform 
 {
+	/**
+	 * Enum definition for PlatformType
+	 * @author Bryan
+	 *
+	 */
 	enum PlatformType
 	{
 		NOTAVAILABLE,PC,XBOX,PS4
 	}
 	
+	
 	private PlatformType m_platform;
 	
+	/**
+	 * Platform default constructor
+	 * 
+	 * Sets platform member to default value
+	 */
 	public Platform()
 	{
 		m_platform = PlatformType.valueOf("NOTAVAILABLE");
 	}
 	
+	/**
+	 * SetPlatform
+	 * Sets platform member variable
+	 * @param platform - Platform value
+	 */
 	public void SetPlatform(String platform)
 	{
 		m_platform = PlatformType.valueOf(platform);
 	}
 	
+	/**
+	 * GetPlatform
+	 * Returns value of platform member
+	 * @return value of platform
+	 */
 	public String GetPlatform()
 	{
 		return m_platform.toString();
