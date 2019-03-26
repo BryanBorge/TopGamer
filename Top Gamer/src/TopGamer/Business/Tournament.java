06e6f5650b007bbac8700c47e4d540f83773af1d
Talha <Talha@TalhasLaptop>
2019-03-26 15:20:12 -0400

    updated tournament class

--- Top Gamer/src/TopGamer/Business/Tournament.java
+++ Top Gamer/src/TopGamer/Business/Tournament.java
@@ -1,6 +1,53 @@
+/*
+ * Tournament class definition
+ * 
+ * @author Talha
+ * 
+ * 
+ */
+
 package TopGamer.Business;
 
 public class Tournament 
 {
+	private String m_TournamentName;
+
+	/*
+	 *constructor for Team class 
+	 * 
+	 * sets member variables to default value
+	 */
 	
+	public Tournament() 
+	{
+		m_TournamentName = "N/A";
+	}
+
+	
+	/*
+	 * sets TournamentName Member Variable
+	 * 
+	 * @param TournamentName
+	 */
+
+	public void setTournamentName(String TournamentName) 
+	{
+		m_TournamentName = TournamentName;
+	}
+
+	/*
+	 * gets TournamentName Member Variable
+	 * 
+	 * @returns TournamentName
+	 */
+
+	public String getTournamentName() 
+	{
+		return m_TournamentName;
+	}
+
+
+
 }
+
+	
